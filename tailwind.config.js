07a84b88bfc06788289f9dc0275e75bdc2628575
Hashim7abdullah <hashim7abdullah7@gmail.com>
2024-12-02 19:32:43 +0530

    changes coming

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gradientColorStops: {
+        'yellow-custom': '#FFD600', // Corresponds to rgba(255,214,0,1)
+        'light-blue-custom': '#CDFCFF', // Corresponds to rgba(205,252,255,1)
+        'gold-custom': '#A08D00', // Corresponds to rgba(160,141,0,1)
+      },
+    },
   },
   plugins: [],
 }
